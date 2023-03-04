bf846bf8ea9cf4fa7f7ceee3fc43e900835ef0bb
shre999 <shreyanshshrey99@gmail.com>
2023-03-04 16:25:19 +0530

    testCommit

--- FileMain.java
+++ FileMain.java
@@ -22,6 +22,7 @@ public class FileMain {
 		//Displaying options for user interaction	
 		System.out.println("1: Retreive File Names in Ascending Order" + "\n" + "2: Business-level Operations" + "\n" + "3: Close the Application" + "\n");
 		System.out.println("Enter Your Choice");
+		System.out.println("Hi ShreyTest");
 		int choice = sc.nextInt();
 		switch(choice)
 		{
